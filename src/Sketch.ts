bfc9ad750cdeb0ec7c56986ab95d7f7c8abf81cd
Rihards Gravis <rihards@gravis.lv>
2022-05-09 22:45:11 +0300

    Audio example

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -1,8 +1,10 @@
 import p5 from "p5";
+import "p5/lib/addons/p5.sound";
 
 export const Sketch = (p: p5) => {
   let w = window.innerWidth;
   let h = window.innerHeight;
+  let mic: p5.AudioIn;
 
   p.windowResized = () => {
     w = window.innerWidth;
@@ -12,11 +14,19 @@ export const Sketch = (p: p5) => {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    mic = new p5.AudioIn();
+    mic.start();
+  };
+
+  p.mouseClicked = () => {
+    p.getAudioContext().resume();
   };
 
   p.draw = () => {
+    const micLevel = mic.getLevel();
+
     p.background(0);
     p.fill([255, 0, 0]);
-    p.circle(w / 2, h / 2, 50);
+    p.circle(w / 2, h / 2, micLevel * 1000 + 50);
   };
 };
---
+++ src/p5.ts
@@ -0,0 +1,4 @@
+import p5 from "p5";
+window.p5 = p5;
+
+export default p5;
